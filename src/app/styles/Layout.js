e9b63928c6b6aaf689bf336d64c72866859e3fbe
js3617 <qkewotjd4574@gmail.com>
2025-04-15 23:44:29 +0900

    Add - style 추가

---
+++ src/app/styles/Layout.js
@@ -0,0 +1,17 @@
+import styled from "styled-components";
+
+const BtnWrap = styled.div`
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    gap: 10px;
+`;
+
+const BtnStartWrap = styled.div`
+    display: flex;
+    justify-content: flex-start;
+    align-items: center;
+    gap: 10px;
+`;
+
+export { BtnWrap, BtnStartWrap };
