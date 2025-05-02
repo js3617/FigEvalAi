6082fe1b56db4f77f3449eaa4cf6a18d9f8b5b3c
js3617 <qkewotjd4574@gmail.com>
2025-05-02 20:23:22 +0900

    Modify - Figma 이미지 삽입 기능 삭제

--- src/plugin/controller.ts
+++ src/plugin/controller.ts
@@ -24,19 +24,20 @@ figma.ui.onmessage = async (msg) => {
     });
   }
 
-  if (msg.type === 'insert-image') {
-    const bytes = await fetch(msg.dataUrl).then(res => res.arrayBuffer());
-    const image = figma.createImage(new Uint8Array(bytes));
-    const rect = figma.createRectangle();
-    rect.resize(200, 200);
-    rect.fills = [{
-      type: 'IMAGE',
-      scaleMode: 'FILL',
-      imageHash: image.hash
-    }];
-    figma.currentPage.appendChild(rect);
-    figma.viewport.scrollAndZoomIntoView([rect]);
-  }
+  //Figma에 이미지 삽입
+  // if (msg.type === 'insert-image') {
+  //   const bytes = await fetch(msg.dataUrl).then(res => res.arrayBuffer());
+  //   const image = figma.createImage(new Uint8Array(bytes));
+  //   const rect = figma.createRectangle();
+  //   rect.resize(200, 200);
+  //   rect.fills = [{
+  //     type: 'IMAGE',
+  //     scaleMode: 'FILL',
+  //     imageHash: image.hash
+  //   }];
+  //   figma.currentPage.appendChild(rect);
+  //   figma.viewport.scrollAndZoomIntoView([rect]);
+  // }
 
   if (msg.type === 'cancel') {
     figma.closePlugin();
