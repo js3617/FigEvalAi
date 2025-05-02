8b9f2816134d2ca5be584941a71ef0f9d5072d21
js3617 <qkewotjd4574@gmail.com>
2025-05-02 20:24:22 +0900

    Add - onReset 버튼 추가

--- src/app/components/Result.tsx
+++ src/app/components/Result.tsx
@@ -1,5 +1,7 @@
 import React from 'react';
+
 import { Btn } from '../styles/Button'
+import { TitleFont } from '../styles/Font'
 
 interface ResultData {
     frameResult: {
@@ -9,21 +11,23 @@ interface ResultData {
     refResults: {
         fileName: string;
         exists: boolean;
+        styles: string[];
     }[];
 }
 
 interface Props {
     data: ResultData;
     onBack: () => void;
+    onReset: () => void;
 }
 
-export default function Result({ data, onBack }: Props) {
+export default function Result({ data, onBack, onReset }: Props) {
     return (
         <div style={{ padding: 20 }}>
             <h3>디자인 유사도 검증 결과</h3>
     
             <div style={{ marginBottom: 24 }}>
-            <p><strong>선택된 Frame 이미지</strong></p>
+            <TitleFont>선택된 Frame 이미지</TitleFont>
             <img
                 src={`http://localhost:3000/uploads/frame/${data.frameResult.fileName}`}
                 alt="frame"
@@ -33,17 +37,18 @@ export default function Result({ data, onBack }: Props) {
     
             {data.refResults.map((res, idx) => (
             <div key={idx} style={{ marginBottom: 32 }}>
-                <p><strong>참고 이미지 {idx + 1}</strong></p>
+                <TitleFont>참고 이미지 {idx + 1}</TitleFont>
                 <img
                 src={`http://localhost:3000/uploads/ref/${res.fileName}`}
                 alt={`ref-${idx}`}
                 style={{ maxWidth: "100%", border: '1px solid #ccc' }}
                 />
-                {/* <p>이미지 존재 여부: {res.exists ? '있음' : '없음'}</p> */}
+                <p>선택한 스타일: {res.styles.join(', ')}</p>
             </div>
             ))}
     
             <Btn onClick={onBack}>이전</Btn>
+            <Btn onClick={onReset}>다시 진행하기</Btn>
         </div>
     );
 }
